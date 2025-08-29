e6fda15149e7bb6206d2f901382fe62cd158212d
khalid-walid <khalidk50050@gmail.com>
2025-08-30 02:53:32 +0300

    Refactor server listen method for consistency and remove unnecessary comments

--- index.js
+++ index.js
@@ -49,8 +49,7 @@ app.use(errorHandler);
 const server = http.createServer(app);
 initSocket(server); // سيبها زي ما هي
 
-// ⚡ استخدام البورت الذي يحدده Fly.io
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, "0.0.0.0", () => {
+const PORT = process.env.PORT || 5000; 
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
-});
+});ئئئئ
