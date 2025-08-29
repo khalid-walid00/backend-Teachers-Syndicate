f70ebab86bfbc2f59b5e06aaa48ab607ec4d5020
khalid-walid <khalidk50050@gmail.com>
2025-08-30 02:17:27 +0300

    Refactor widget controller imports to use lowercase directory names for consistency

--- routes/admin/widgets/index.js
+++ routes/admin/widgets/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
-const { getAllWidgets, getOneWidget, getWidgetsByCategory } = require('../../../controllers/admin/Widget/getWidgets.controller');
-const { updateWidget } = require('../../../controllers/admin/Widget/updateWidget.controller');
-const { createWidget } = require('../../../controllers/admin/Widget/createWidget.controller');
-const { deleteWidget } = require('../../../controllers/admin/Widget/deleteWidget.controller');
+const { getAllWidgets, getOneWidget, getWidgetsByCategory } = require('../../../controllers/admin/widget/getWidgets.controller');
+const { updateWidget } = require('../../../controllers/admin/widget/updateWidget.controller');
+const { createWidget } = require('../../../controllers/admin/widget/createWidget.controller');
+const { deleteWidget } = require('../../../controllers/admin/widget/deleteWidget.controller');
 const upload = require('../../../middleware/upload.middleware');
 var router = express.Router();
 
